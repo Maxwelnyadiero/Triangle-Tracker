7ba3f470e2bc35e1f7305cc59edeb0770dbd5ae0
rosemarymagoma99@gmail.com <maxwelnyadiero@gmail.com>
2018-12-02 19:30:51 +0300

    added link to js

--- Triangle/js/script.js
+++ Triangle/js/script.js
@@ -1,20 +1,6 @@
-function finalResult(){
-  var u = parseInt(document.getElementById('u').value);
-  var v = parseInt(document.getElementById('v').value);
-  var w =parseInt(document.getElementById('w').value);
-  var answers = document.getElementById('answerResult');
-  if(u==v>w && v==w> && u==v>w ){
-    if(u==v && u==w){
-      answerResult.textContent = 'Equilateral:All sides equal!';
-    }
-    else if(u===v||v===w||u===w){
-      answerResult.textContent = 'isosceles:Two sides equal!';
-    }
-    else{
-      answerResult.textContent = 'scalene:No side equal!';
-    }
-  }
-  else{
-    answerResult.textContent = 'Not atriangle!';
-  }
+var theMorning = true;
+if (theMorning) {
+  console.log("I should wake up!")
+} else{
+  console.log("continue sleeping!");
 }
